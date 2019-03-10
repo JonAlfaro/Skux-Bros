2648261ed69297291d5a41f48fae1791168a2641
Jon Alfaro <ljjalfaro@gmail.com>
2019-03-10 13:55:32 +1000

    😂😍😘

---
+++ Assets/Scripts/DummyPlayerControlledVelocity.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyPlayerControlledVelocity : MonoBehaviour
+{
+    [SerializeField]
+    Vector2 v2Force;
+    [SerializeField]
+    KeyCode keyPositive;
+    [SerializeField]
+    KeyCode keyNegative;
+    private Rigidbody2D rgb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(keyPositive))
+            rgb.velocity += v2Force;
+
+        if (Input.GetKey(keyNegative))
+            rgb.velocity -= v2Force;
+    }
+}
---
+++ Assets/Scripts/LevelDeath.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelDeath : MonoBehaviour
+{
+    [SerializeField]
+    string strTag;
+
+    void Start()
+    {
+        Debug.Log("STARTING");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == strTag)
+        {
+            collision.gameObject.transform.position = new Vector2(0, 5);
+        }
+    }
+}
