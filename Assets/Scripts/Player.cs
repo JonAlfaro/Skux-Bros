8ce78e7c100e29b2204c6d62a1e6ba25f95b0a37
yung lean <33080132+Michael23B@users.noreply.github.com>
2019-03-10 18:33:49 +1000

    Added scuffed animation

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+/**
+ * Currently just handles animation stuff :s
+ */
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb = null;
+    private Animator animator = null;
+    private CoolJumps coolJumps = null;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        coolJumps = GetComponent<CoolJumps>();
+    }
+
+    void Update()
+    {
+        DoAnimationStuff();
+    }
+
+    void DoAnimationStuff()
+    {
+        animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.Play("Player_Jump");
+        }
+
+        animator.SetBool("Crouching", Input.GetButton("Crouch"));
+
+        if (Input.GetMouseButton(1)) animator.Play("Player_Attack");
+    }
+}
