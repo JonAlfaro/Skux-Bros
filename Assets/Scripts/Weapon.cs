4a6bdf3f5121b7c50ac9a1251ae853a8621e59be
Jon Alfaro <ljjalfaro@gmail.com>
2019-03-12 19:30:46 +1000

    projectile Scripts added, with legal sfx

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public float offset;
+
+    public GameObject projectile;
+    public Transform shotPoint;
+    private float timeBetweenShots;
+    public float startTimeBetweenShots;
+    public AudioSource gunSound;
+    public AudioClip gunSoundX;
+
+    void Start()
+    {
+        gunSound.clip = gunSoundX;
+    }
+    private void Update()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+
+        if (timeBetweenShots <= 0)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Instantiate(projectile, shotPoint.position, transform.rotation);
+                gunSound.Play();
+                timeBetweenShots = startTimeBetweenShots;
+            }
+        }
+        else
+        {
+            timeBetweenShots -= Time.deltaTime;
+        }
+
+    }
+}
